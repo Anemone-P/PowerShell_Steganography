189a7b0cf88133fdcb07122036201b7a8fac837c
Anemone-P <49353680+Anemone-P@users.noreply.github.com>
2025-12-02 19:58:28 -0600

    Create Decoder.ps1
    Used to decode data from images created with the Encoder.ps1

---
+++ Decoder.ps1
@@ -0,0 +1,74 @@
+function Decode-PNG {
+    param (
+            [Parameter(Mandatory=$true, ValueFromPipeline=$true)] [string]$ImagePath
+        )
+    function ConvertTo-Hex {
+        param([String]$TextToConvert)
+        $hexBytes = $TextToConvert | Format-Hex | Select-Object -ExpandProperty Bytes
+        $rawHexString = ($hexBytes | ForEach-Object { '{0:x2}' -f $_ }) -join ''
+        $rawHexString
+    }
+
+    function ConvertFrom-Hex {
+        param (
+            [Parameter(Mandatory=$true, ValueFromPipeline=$true)] [string]$HexString
+        )
+        if ($HexString.Length % 2 -ne 0) {
+            Write-Error "Character count isn't even!"
+            return
+        }
+        $byteArray = New-Object byte[] ($HexString.Length / 2)
+        for ($i = 0; $i -lt $HexString.Length; $i += 2) {
+            $hexPair = $HexString.Substring($i, 2)
+            $byteArray[$i / 2] = [Convert]::ToByte($hexPair, 16)
+        }
+        [System.Text.Encoding]::UTF8.GetString($byteArray)
+    }
+
+    Add-Type -AssemblyName System.Drawing
+    $bitmap = New-Object System.Drawing.Bitmap($imagePath)
+    $width = $bitmap.Width
+    $height = $bitmap.Height
+    $TotalPixels = $width*$height
+
+    # Create an array that includes pixel data for the first 200 x 50 pixels
+    $PixelData=@()
+    for ($x = 0; $x -lt $bitmap.Width; $x++) {
+        for ($y = 0; $y -lt $bitmap.Height; $y++) {
+            if ($x -lt 200 -and $y -lt 50) {
+                $currentPixelData = $bitmap.GetPixel($x, $y)
+                $Pixel = [PSCustomObject]@{
+                        X               = $x
+                        Y               = $Y
+                        R               = $currentPixelData.R
+                        G               = $currentPixelData.G
+                        B               = $currentPixelData.B
+                        A               = $currentPixelData.A
+                        IsKnownColor    = $currentPixelData.IsKnownColor
+                        IsEmpty         = $currentPixelData.IsEmpty
+                        IsNamedColor    = $currentPixelData.IsNamedColor
+                        Name            = $currentPixelData.Name
+                }
+                $PixelData += $Pixel
+            }        
+        }    
+    }
+
+    $ToCheck = $PixelData | Where-Object {$_.A -lt 255}
+    $Charray = @("N","0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f")
+    $HexFound = @()
+    $i=0
+    foreach($pixel in $ToCheck){
+        if($i % 2 -eq 0){
+            $ADif = (255 - $pixel.A) -1
+            $ADifNext = (255 - $tocheck[$i+1].A) -1
+            $blah = "$ADif"+"$ADifNext"
+            $AdifHex = $charray[$blah]
+            $HexFound += $AdifHex
+        }
+        $i=$i+1
+    }
+
+    $HexConverted = $HexFound  -join ""  | ConvertFrom-Hex
+    write-host $HexConverted
+ }
